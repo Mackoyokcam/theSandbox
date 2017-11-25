5f30fdb87f7884faa19ec09116f5b42ae380d18f
mackoyokcam <mark.hangarter@gmail.com>
2017-11-24 23:46:55 -0800

    All i need?

---
+++ model/account.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const crypto = require('crypto');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const httpErrors = require('http-errors');
+
+const accountSchema = mongoose.Schema({
+  username: {type: String, required: true, unique: true},
+  email: {type: String, required: true, unique: true},
+  passwordHash: {type: String, required: true},
+  googleOAuth: {type: Boolean, default: false},
+  tokenSeed: {type: String, required: true, unique: true},
+  created: {type: Date, default: () => new Date()},
+});
+
+// Instance methods
+accountSchema.methods.passwordVerify = function(password) {
+  return bcrypt.compare(password, this.passwordHash)
+    .then(correctPassword => {
+      if(!correctPassword)
+        throw httpErrors(401, '__AUTH_ERROR__ incorrect password');
+      return this;
+    });
+};
+
+accountSchema.methods.tokenCreate = function() {
+  this.tokenSeed = crypto.randomBytes(64).toString('hex');
+  return this.save()
+    .then(account => {
+      let options = {expiresIn: '7d'};
+      return jwt.sign({tokenSeed: account.tokenSeed}, process.env.CLOUD_SECRET, options);
+    });
+};
+
+accountSchema.methods.update = function(data) {
+  let {password} = data;
+  delete data.password;
+  return bcrypt.hash(password, 8)
+    .then(passwordHash => {
+      this.username = data.username;
+      this.email = data.email;
+      this.passwordHash = passwordHash;
+      return this.save();
+    });
+};
+
+const Account = module.exports = mongoose.model('account', accountSchema);
+
+// Data is going to contain {username, email, and password}
+Account.create = function(data) {
+  // Hash password
+  let {password} = data;
+  delete data.password;
+  return bcrypt.hash(password, 8)
+    .then(passwordHash => {
+      data.passwordHash = passwordHash;
+      // Generate a tokenSeed
+      data.tokenSeed = crypto.randomBytes(64).toString('hex');
+      return new Account(data).save();
+    });
+};
+
+Account.handleGoogleOAuth = function(openIDProfile){
+  return Account.findOne({email: openIDProfile.email})
+    .then(account => {
+      if (account) {
+        if(account.googleOAuth)
+          return account;
+        throw new Error('account found but not connected to google');
+      }
+      // create an account based on the email
+      return new Account({
+        username: openIDProfile.email.split('@')[0],
+        email: openIDProfile.email,
+        passwordHash: crypto.randomBytes(32).toString('hex'),
+        tokenSeed: crypto.randomBytes(32).toString('hex'),
+        googleOAuth: true,
+      })
+        .save();
+    });
+};
---
+++ route/auth-router.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const {Router} = require('express');
+const Account = require('../model/account.js');
+const httpErrors = require('http-errors');
+const basicAuth = require('../lib/basic-auth-middleware.js');
+const superagent = require('superagent');
+
+const authRouter = module.exports = new Router();
+
+authRouter.post('/auth', (req, res, next) => {
+  Account.create(req.body)
+    .then(account => account.tokenCreate())
+    .then(token => {
+      res.cookie('X-Charity-Token', token, {maxAge: 604800000});
+      res.json({token});
+    })
+    .catch(next);
+});
+
+authRouter.get('/auth', basicAuth, (req, res, next) => {
+  req.account.tokenCreate()
+    .then(token => {
+      res.cookie('X-Charity-Token', token, {maxAge: 604800000});
+      res.json({token});
+    })
+    .catch(next);
+});
+
+authRouter.put('/auth', basicAuth, (req, res, next) => {
+
+  if (!req.body.username || !req.body.email || !req.body.password)
+    return next(httpErrors(400, '__REQUEST_ERROR__ username, email, and password required'));
+
+  req.account.update(req.body)
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch(next);
+});
+
+authRouter.get('/oauth/google', (req, res) => {
+  if(!req.query.code){
+    res.redirect(process.env.CLIENT_URL);
+  } else {
+    console.log('get token');
+    superagent.post('https://www.googleapis.com/oauth2/v4/token')
+      .type('form')
+      .send({
+        code: req.query.code,
+        grant_type: 'authorization_code',
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_secret: process.env.GOOGLE_CLIENT_SECRET,
+        redirect_uri: `${process.env.API_URL}/oauth/google`,
+      })
+      .then((res) => {
+        if(!res.body.access_token)
+          throw new Error('no access token');
+        return res.body.access_token;
+      })
+      .then(accessToken => {
+        console.log('OpenID');
+        return superagent.get('https://www.googleapis.com/plus/v1/people/me/openIdConnect')
+          .set('Authorization', `Bearer ${accessToken}`);
+      })
+      .then(res => {
+        console.log('handle google auth');
+        // find an account or create an account
+        return Account.handleGoogleOAuth(res.body);
+      })
+      .then(account => account.tokenCreate())
+      .then(token => {
+        res.cookie('X-Charity-Token', token, {maxAge: 604800000});
+        res.redirect(process.env.CLIENT_URL);
+      })
+      .catch(err => {
+        console.error(err);
+        res.cookie('X-Charity-Token', '');
+        res.redirect(process.env.CLIENT_URL + '?error=oauth');
+      });
+  }
+});
